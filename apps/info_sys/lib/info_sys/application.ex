9e700b36a2ab022685e6ce18b4176ef7f254bcfb
sumanp <sumanpuri55@gmail.com>
2021-05-19 03:50:21 +0530

    Added supervision strategy to sample OTP GenServer

--- apps/info_sys/lib/info_sys/application.ex
+++ apps/info_sys/lib/info_sys/application.ex
@@ -10,6 +10,10 @@ defmodule InfoSys.Application do
     children = [
       # Starts a worker by calling: InfoSys.Worker.start_link(arg)
       # {InfoSys.Worker, arg}
+      Supervisor.child_spec({InfoSys.Counter, 5}, restart: :permanant)
+      # module you want to start and the value that will be received on start_link
+      # by the GenServer. Alternatively, passing in only a module name uses a default
+      # value of [].
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
